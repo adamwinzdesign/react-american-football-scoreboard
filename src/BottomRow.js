2e6e9ef890ceee4c36007b1147ea3e6834393211
adamwinzdesign <adamwinzdesign@gmail.com>
2019-10-07 15:13:07 -0500

    added ability to change down and quarter

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const BottomRow = () => {
+  const [down, setDown] = useState(1);
+  const [qtr, setQtr] = useState(1);
+
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">{down}</div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
@@ -18,8 +21,22 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{qtr}</div>
       </div>
+      <button className="homeButtons__touchdown" onClick={() => {
+        if(down < 4) {
+          setDown(down + 1)
+        } else {
+          setDown(1)
+        }
+          }}>Advance Down</button>
+      <button className="homeButtons__touchdown" onClick={() => {
+        if(qtr< 4) {
+          setQtr(qtr + 1)
+        } else {
+          setQtr(1)
+        }
+          }}>Advance qtr</button>
     </div>
   );
 };
